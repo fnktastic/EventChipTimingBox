a8c26a69297bb60d868b858bb8fe63f75a026615
Alex Fnktastic <fnktastic@outlook.com>
2018-12-22 01:10:19 +0200

    IP address fix

--- ECTL/TcpFileSender.cs
+++ ECTL/TcpFileSender.cs
@@ -10,42 +10,34 @@ namespace ECTL
 {
     public class TcpFileSender
     {
-        const int PORT_NO = 5000;
-        const string SERVER_IP = "127.0.0.1";
+        const int PORT = 5000;
 
         public static void Start()
         {
-                //---listen at the specified IP and port no.---
-                IPAddress localAdd = IPAddress.Parse(SERVER_IP);
-                TcpListener listener = new TcpListener(localAdd, PORT_NO);
-                TcpClient tcpClient = new TcpClient();
-                Console.WriteLine("Listening...");
-                listener.Start();
+            IPAddress localAdd = IPAddress.Any;
+            TcpListener listener = new TcpListener(localAdd, PORT);
+            TcpClient tcpClient = new TcpClient();
+            Console.WriteLine("Listening...");
+            listener.Start();
             try
             {
                 while (true)
                 {
-                    //---incoming client connected---
                     tcpClient = listener.AcceptTcpClient();
 
-                    //---get the incoming data through a network stream---
                     NetworkStream nwStream = tcpClient.GetStream();
                     byte[] buffer = new byte[tcpClient.ReceiveBufferSize];
 
-                    //---read incoming stream---
                     int bytesRead = nwStream.Read(buffer, 0, tcpClient.ReceiveBufferSize);
 
-                    //---convert the data received into a string---
                     string fileName = Encoding.ASCII.GetString(buffer, 0, bytesRead);
                     Console.WriteLine("Received : " + fileName);
 
-                    //---write back the text to the client---
                     Console.WriteLine("Sending buffer size");
                     var bytes = ReadFileByName(fileName);
                     var bytesLenght = ASCIIEncoding.ASCII.GetBytes(bytes.Length.ToString());
                     nwStream.Write(bytesLenght, 0, bytesLenght.Length);
 
-                    //---write back the text to the client---
                     Console.WriteLine("Sending back Reads");
                     nwStream.Write(bytes, 0, bytes.Length);
                 }
