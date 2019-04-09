999e9f2bc8967f9127a317c84196fcb90955bb5e
fnktastic <VillaNaranjos@yandex.ua>
2019-04-09 18:25:23 +0300

    Added Wcf Service

---
+++ Services/Server/Consts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Server
+{
+    public static class Consts
+    {
+        public const int DELAY = 3750;
+        private const string HTTP_SERVICE_PATH = "Service.svc";
+        private const string TCP_SERVICE_PATH = "Design_Time_Addresses/ATWService/Service/tcp";
+
+        public static string HttpUrl(int port = 81, string host = "77.68.12.158")
+        {
+            return string.Format("http://{0}/{2}", host, port, HTTP_SERVICE_PATH);
+        }
+
+        public static string HttpLocalhost(int port = 81, string host = "localhost")
+        {
+            return string.Format("http://{0}/{2}", host, port, HTTP_SERVICE_PATH);
+        }
+
+        public static string TcpUrl(int port = 8732, string host = "192.168.1.101")
+        {
+            return string.Format("net.tcp://{0}:{1}/{2}", host, port, TCP_SERVICE_PATH);
+        }
+    }
+}
---
+++ Services/Server/ServerService.cs
@@ -0,0 +1,63 @@
+﻿using AtwService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Server
+{
+    public class ServerService
+    {
+        private dynamic binding;
+        private dynamic endpoint;
+
+        public async Task SendAsync()
+        {
+            using (var channelFactory = new ChannelFactory<IService>(binding, endpoint))
+            {
+                channelFactory.Credentials.UserName.UserName = string.Empty;
+                channelFactory.Credentials.UserName.Password = string.Empty;
+
+                IService service = null;
+                try
+                {
+                    service = channelFactory.CreateChannel();
+                    var reader = await service.SetReaderAsync(new Reader());
+                    var reading = await service.SetReadingAsync(new Reading() { Id = Guid.NewGuid(), ReaderId = reader.Id, IPAddress = "192.168.15.125", StartedDateTime = DateTime.UtcNow });
+
+                        var read = await service.SetReadAsync(new Read() { Id = Guid.NewGuid(), ReadingId = reading.Id, EPC = "TAG 14", Time = DateTime.UtcNow });
+
+                        await Task.Delay(Consts.DELAY);
+                }
+                catch (Exception ex)
+                {
+                    (service as ICommunicationObject)?.Abort();
+                    Logger.Log.Error(string.Format("{0}: {1}", nameof(SendAsync), ex.Message));
+                }
+            }
+        }
+
+        private void InitProtocol(ProtocolEnum protocol)
+        {
+            try
+            {
+                if (protocol == ProtocolEnum.Http)
+                {
+                    binding = new WSHttpBinding(SecurityMode.None);
+                    endpoint = new EndpointAddress(Consts.HttpUrl());
+                }
+                if (protocol == ProtocolEnum.Tcp)
+                {
+                    binding = new NetTcpBinding(SecurityMode.None);
+                    endpoint = new EndpointAddress(Consts.TcpUrl());
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log.Error(string.Format("{0}: {1}", nameof(InitProtocol), ex.Message));
+            }
+        }
+    }
+}
